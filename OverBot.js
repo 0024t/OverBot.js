ac86f9cd7a5ba9cc04c5ca750bc5e3da034b7194
Edorion <21106616+0024t@users.noreply.github.com>
2018-03-30 20:22:49 +0200

    Translation
    Translated french into english for more readability

--- OverBot.js
+++ OverBot.js
@@ -6,19 +6,19 @@ var pseudo;
 const bot = new Discord.Client()
 var KDA = 0;
 
-bot.login(process.argv[2]) //token du bot
+bot.login(process.argv[2]) //get the bot token
 
 bot.on("ready", function () {
   console.log("Connected")
 })
 
 bot.on("message", message => {
-  console.log(message.guild + "  :  " + message.author.username + ": " + message); //affiche la guilde suivie de l'auteur et du message dans les logs
+  console.log(message.guild + "  :  " + message.author.username + ": " + message); //Write server's messages on the console log
 
   if (message.content.startsWith("OwStat")) { 
     pseudo = message.content.split(" ", 2);
     message.delete
-    owjs //fonction pour récuperer les données
+    owjs //Main function
       .getAll('pc', 'eu', pseudo[1].split("#").join("-"))
       .then((data) => message.channel.send({
         embed: {
@@ -76,7 +76,7 @@ bot.on("message", message => {
       }))
   }
 
-  if (message.content.startsWith("OverBot shutdown")) { //éteint le bot
+  if (message.content.startsWith("OverBot shutdown")) { //Shutdown the bot
     message.channel.sendMessage("Disconnected")
     bot.user.exit;
     process.exit(0)
@@ -88,7 +88,7 @@ function addition(Value1, Value2) { //Addition
   return result
 }
 
-function IsNull(Data) { //vérifie la valeur d'une variable. Si elle est nulle, retourne un Nombre 0
+function IsNull(Data) { //Check the value of a variable
     if (typeof Data != "undefined") {
       return Data
     } else {
@@ -96,12 +96,12 @@ function IsNull(Data) { //vérifie la valeur d'une variable. Si elle est nulle, 
     }
 }
 
-function division(Value1, Value2) { //Fait une division avec moins de texte ^^
+function division(Value1, Value2) { //Division
   result = Value1 / Value2
   return result;
 }
 
-function retournerunstring(Value) { //Retourne un String à partir d'un nombre
+function retournerunstring(Value) { //Return string from a number
   KDA = Value;
   KDA = KDA.toFixed(2);
   return KDA.toString();
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # OverBot.js
+Welcome to the best overwatch stat tracker for discord !!!
+
+To launch it, install node.js, and start the bot with the command : node LOCATION_OF_THE_BOT\OverBot.js YOUR_BOT_TOKEN
+Have fun !!
